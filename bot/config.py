90f67f79052b6be23c81e222925f196df6e0cc9b
Eren Yeager <107749289+Eren-YeagerO@users.noreply.github.com>
2023-07-30 02:47:15 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,15 +1,16 @@
 import os
 
 class Config:
-    TELEGRAM_TOKEN=os.environ['TELEGRAM_TOKEN']
-    TELEGRAM_APP_HASH=os.environ['TELEGRAM_APP_HASH']
-    TELEGRAM_APP_ID=int(os.environ['TELEGRAM_APP_ID'])
-    
+    TELEGRAM_TOKEN = '5782470033:AAFvagcCui2IGFeB1Utp2UgMTZrzDqZuWsQ'
+    TELEGRAM_APP_HASH = '9b5ff18e133fed80f0f6cd7bf30cffdd'
+    TELEGRAM_APP_ID = 13715737
+
     if not TELEGRAM_TOKEN:
         raise ValueError('TELEGRAM BOT TOKEN not set')
-    
+
     if not TELEGRAM_APP_HASH:
         raise ValueError("TELEGRAM_APP_HASH not set")
 
     if not TELEGRAM_APP_ID:
         raise ValueError("TELEGRAM_APP_ID not set")
+        
