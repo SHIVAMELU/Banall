4ddd75344c0650c136a61c41dd3dbc5b852bf9e2
Eren Yeager <107749289+Eren-YeagerO@users.noreply.github.com>
2023-10-18 02:34:12 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    TELEGRAM_TOKEN = '6148628677:AAFefqABCckXtVqnCk-q4SHNaga8duK-07s'
+    TELEGRAM_TOKEN = '1463915823:AAHv2gne2WVd-fIfcBhOPU1CuZm__4KyOrc'
     TELEGRAM_APP_HASH = '9b5ff18e133fed80f0f6cd7bf30cffdd'
     TELEGRAM_APP_ID = 13715737
 
